d0b32d68eb22dbe33d8c859c8c9333455befb3d8
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-11-11 07:04:40 -0700

    tenant modals - initial

---
+++ public/javascript/tenant-modals.js
@@ -0,0 +1,27 @@
+// Get Modals from Tenant-Dashboard Handlebars
+const maintenanceModal = document.getElementById("maintenance-modal");
+const petModal = document.getElementById("pet-modal");
+
+// Display Maintenance Request Form (Modal)
+const maintenanceBeginRequest = function() {
+    maintenanceModal.style.display = "block";
+}
+
+// Display Update Pet Status Form (Modal)
+const petBeginRequest = function() {
+    petModal.style.display = "block";
+}
+
+// Close Modals
+function closeModal() {
+    maintenanceModal.style.display = "none";
+    petModal.style.display = "none";
+};
+
+
+
+// Maintenance Modal Event Listener
+document.querySelector("#maintenance-begin-request").addEventListener("click", maintenanceBeginRequest);
+
+// Pet Modal Event Listener
+document.querySelector("#pet-begin-request").addEventListener("click", petBeginRequest);

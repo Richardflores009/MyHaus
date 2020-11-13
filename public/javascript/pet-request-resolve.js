09ff69884317f95df61fc5f7c89554e90e5ea134
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-11-13 02:15:16 -0700

    pet requests display

---
+++ public/javascript/pet-request-resolve.js
@@ -0,0 +1,7 @@
+// LANDLORD RESOLVES PET REQUEST (deletes from database)
+const resolvePetRequest = function() {
+    console.log("resolved")
+};
+
+//Event Listener for Resolve Request
+document.querySelector("#resolvePetRequest").addEventListener("click", resolvePetRequest);
